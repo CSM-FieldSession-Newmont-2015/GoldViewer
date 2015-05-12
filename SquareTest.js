77032981bca978346a22fdb9cfe95feddf638992
Chris <chrbutle@mines.edu>
2015-05-12 14:51:30 -0600

    Added shadows and lighting. Added a floor for the box to 'sit' on. Commented things to make life easier. Added cylinders placed randomly within the property box for stress trests.
    Everything works fine in Chrome and Firefox. IE11 is broken and Safari
    untested.

--- SquareTest.js
+++ SquareTest.js
@@ -1,47 +1,74 @@
-
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-//Resize the camera when the window is resized
+// Resize the camera when the window is resized
 window.addEventListener('resize', function(event){
 		camera.aspect = window.innerWidth / window.innerHeight;
 		camera.updateProjectionMatrix();
-		renderer.setSize( window.innerWidth, window.innerHeight );
+		renderer.setSize(window.innerWidth, window.innerHeight);
 		render();
 	});
 
-controls = new THREE.OrbitControls( camera );
-controls.addEventListener( 'change', render );
+// This brings in basic controls for our view, like rotating, panning, and zooming.
+controls = new THREE.OrbitControls(camera);
+controls.addEventListener('change', render);
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setClearColor(0x252525, 1);
-document.body.appendChild( renderer.domElement );
-
-var geometry = new THREE.CylinderGeometry(0.05, 0.05, 0.1, 10);
-geometry.computeFaceNormals();
-geometry.computeVertexNormals();
-
-var material = new THREE.MeshLambertMaterial({
-	color: 0xff00ff,
-	side: 2,
-	shading: THREE.FlatShading,
+var renderer = new THREE.WebGLRenderer({
+	antialias: true
 });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0xeeeeee, 1);
+// Enable shadow stuff.
+renderer.shadowMapEnabled = true;
+renderer.shadowMapType = THREE.PCFSoftShadowMap;
 
-for (var i = 0; i < 100; i += 1) {
-	var matrix = new THREE.Matrix4().makeTranslation(10*Math.random(), 10*Math.random(), 10*Math.random());
-	matrix.matrixAutoUpdate = false;
+document.body.appendChild(renderer.domElement);
 
-	var cylinder = new THREE.Mesh( geometry, material );
-	cylinder.applyMatrix(matrix);
+// Generate a bunch of cylinders in random locations, within the box.
+var cylinder_geometry = new THREE.CylinderGeometry(0.05, 0.05, 0.3, 10);
+var pink_material = new THREE.MeshLambertMaterial({color: 0xff00ff});
+
+for (var i = 0; i < 200; i += 1) {
+	var cylinder = new THREE.Mesh(cylinder_geometry, pink_material);
+	// We multiply by 10 because the property_boundary size is 10.
+	cylinder.position.x = 10*Math.random();
+	cylinder.position.y = 10*Math.random();
+	cylinder.position.z = 10*Math.random();
+	cylinder.castShadow = true;
 	scene.add(cylinder);
 }
 
-var box = new THREE.BoxHelper(new THREE.Mesh(new THREE.BoxGeometry(10, 10, 10)));
-box.material.color.setRGB( 1, 1, 1);
-box.applyMatrix( new THREE.Matrix4().makeTranslation(5, 5, 5) );
+// This creates a wireframe box to encompass the property, but without the diagonals.
+var property_boundary = new THREE.Mesh(new THREE.BoxGeometry(10, 10, 10));
+var box = new THREE.EdgesHelper(property_boundary, 0x5d5d5d);
+box.applyMatrix(new THREE.Matrix4().makeTranslation(5, 5, 5));
 scene.add(box);
+
+// This creates a floor of sorts for everything to sit on. It helps orient oneself.
+var plane = new THREE.Mesh(new THREE.PlaneBufferGeometry(35, 35),
+	                       new THREE.MeshLambertMaterial({color: 0xffffff}));
+plane.receiveShadow = true;
+// Render both sides. Otherwise, it goes invicible.
+plane.material.side = THREE.DoubleSide;
+// We want the plane flat in the xz plane.
+plane.rotation.x = -0.5*Math.PI;
+// Position *just* below the cube, so the cube and plane don't compete for the screen.
+plane.position.y = -0.1; 
+// Center the plane under the center of the cube.
+plane.position.x = 5;
+plane.position.z = 5;
+scene.add(plane);
+
+// A spotlight gives us shadows.
+var spotlight = new THREE.SpotLight(0xffffff);
+// This position is pretty arbitrary. It looks nice here, so I put it here.
+spotlight.position.set(-35, 80, -35);
+spotlight.castShadow = true;
+scene.add(spotlight);
+
+// And ambiant light ensures every face is lit, even if it's just a little.
+scene.add(new THREE.AmbientLight(0x404040));
 
 //********** Surface ***********
 			var gridSize = 11;
@@ -67,11 +94,11 @@ scene.add(box);
 			elevation.applyMatrix(new THREE.Matrix4().makeTranslation(0, 8, 0));
 			elevation.computeBoundingBox();
 
-			var material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+			var material = new THREE.MeshBasicMaterial({ color: 0x006600 });
 			material.wireframe = true;
-			var surface = new THREE.Mesh( elevation, material );
+			var surface = new THREE.Mesh(elevation, material);
 			surface.position.y = -5;
-			scene.add( surface );
+			scene.add(surface);
 
 			camera.position.z = 5;
 			camera.position.y = 5;
@@ -83,6 +110,6 @@ camera.position.y = 20;
 camera.position.z = 20;
 
 function render() {
-	renderer.render( scene, camera );
+	renderer.render(scene, camera);
 }
 render();
