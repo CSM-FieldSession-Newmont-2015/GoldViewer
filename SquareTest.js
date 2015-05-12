c3b0cbe587fed39d14051b5d77d391dec76d6e90
unknown <avanderm@MZ022-35.adit.mines.edu>
2015-05-12 10:47:38 -0600

    Fixed camera when window is resized

--- SquareTest.js
+++ SquareTest.js
@@ -2,6 +2,14 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+
+//Resize the camera when the window is resized
+window.addEventListener('resize', function(event){
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+		renderer.setSize( window.innerWidth, window.innerHeight );
+	});
+
 controls = new THREE.OrbitControls( camera );
 controls.addEventListener( 'change', render );
 
