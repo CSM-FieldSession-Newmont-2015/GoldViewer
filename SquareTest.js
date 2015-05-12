95a23cde4a80216e857bc00750d8885c98f10891
Chris <chrbutle@mines.edu>
2015-05-12 12:42:11 -0600

    Changed FOV to 50.

--- SquareTest.js
+++ SquareTest.js
@@ -1,6 +1,6 @@
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 
 //Resize the camera when the window is resized
