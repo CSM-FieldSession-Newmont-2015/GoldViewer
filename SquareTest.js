ee7f7b6b394cc991390eb94f9377f3e77081a5c5
Joseph <jostockw@mines.edu>
2015-05-12 12:43:00 -0600

    Added surface mesh and recentering

--- SquareTest.js
+++ SquareTest.js
@@ -28,6 +28,40 @@ var box = new THREE.BoxHelper(new THREE.Mesh(new THREE.BoxGeometry(10, 10, 10)))
 box.material.color.setRGB( 1, 1, 1);
 scene.add(box);
 
+//********** Surface ***********
+			var gridSize = 8;
+
+			var elevation = new THREE.Geometry();
+			for (i = 0; i < gridSize; ++i) {
+			    for (j = 0; j < gridSize; ++j) {
+			        elevation.vertices.push(
+                        new THREE.Vector3(i, Math.random(), j)
+                        );
+			    }
+			}
+
+			for (i = 0; i < gridSize - 1; ++i) {
+			    for (j = 0; j < gridSize - 1; ++j) {
+			        elevation.faces.push(
+                        new THREE.Face3(j + i * gridSize, j + 1 + i * gridSize, j + (i + 1) * gridSize),
+                        new THREE.Face3(j + (i + 1) * gridSize, j + 1 + i * gridSize, j + 1 + (i + 1) * gridSize)
+                        );
+			    }
+			}
+
+			elevation.applyMatrix( new THREE.Matrix4().makeTranslation(-gridSize/2, 0, -gridSize/2) );
+			elevation.computeBoundingBox();
+
+			var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+			material.wireframe = true;
+			var surface = new THREE.Mesh( elevation, material );
+			scene.add( surface );
+
+			camera.position.z = 5;
+			camera.position.y = 5;
+			camera.lookAt(new THREE.Vector3(0,0,0));
+
+//******************************
 camera.position.z = 5;
 
 function render() {
