8fac09c5df7ee25b1c321c0c7fd94d529daa1f9d
Alan VanderMeer <avanderm@mymail.mines.edu>
2015-05-13 16:31:46 -0600

    Added labels for the axis

--- SquareTest.js
+++ SquareTest.js
@@ -112,34 +112,42 @@ var surface = new THREE.Mesh(elevation, new THREE.MeshBasicMaterial({
 surface.position.set(0, 7, 0);
 scene.add(surface);
 
-var spritey = makeTextSprite( " 				Hello, World",
-		{ fontsize: 18, borderColor: {r:255, g:0, b:0, a:1.0}, backgroundColor: {r:255, g:100, b:100, a:0.8} } );
-	spritey.position.set(0,0,0);
+//Add labels for the axis hardcoded position currently
+var spritey = makeTextSprite( "Z",
+		{ fontsize: 25,size: 250, borderColor: {r:255, g:0, b:0, a:1.0}, backgroundColor: {r:255, g:100, b:100, a:0.8} } );
+	spritey.position.set(0,11,0);
+	scene.add( spritey );
+var spritey = makeTextSprite( "X",
+		{ fontsize: 25,size: 250, borderColor: {r:255, g:0, b:0, a:1.0}, backgroundColor: {r:255, g:100, b:100, a:0.8} } );
+	spritey.position.set(0,0,11);
+	scene.add( spritey );
+var spritey = makeTextSprite( "Y",
+		{ fontsize: 25,size: 250, borderColor: {r:255, g:0, b:0, a:1.0}, backgroundColor: {r:255, g:100, b:100, a:0.8} } );
+	spritey.position.set(11,0,0);
 	scene.add( spritey );
 
 function makeTextSprite( message, parameters ) {
 		if ( parameters === undefined ) parameters = {};
 		var fontface = parameters.hasOwnProperty("fontface") ? parameters["fontface"] : "Arial";
 		var fontsize = parameters.hasOwnProperty("fontsize") ? parameters["fontsize"] : 18;
+		var size = parameters.hasOwnProperty("size") ? parameters["size"] : 100;
 		var borderThickness = parameters.hasOwnProperty("borderThickness") ? parameters["borderThickness"] : 4;
 		var borderColor = parameters.hasOwnProperty("borderColor") ?parameters["borderColor"] : { r:0, g:0, b:0, a:1.0 };
 		var backgroundColor = parameters.hasOwnProperty("backgroundColor") ?parameters["backgroundColor"] : { r:255, g:255, b:255, a:1.0 };
 		var textColor = parameters.hasOwnProperty("textColor") ?parameters["textColor"] : { r:0, g:0, b:0, a:1.0 };
 
 		var canvas = document.createElement('canvas');
+		canvas.width=size;
+		canvas.height=size;
 		var context = canvas.getContext('2d');
 		context.font = "Bold " + fontsize + "px " + fontface;
 		var metrics = context.measureText( message );
 		var textWidth = metrics.width;
 
-		context.fillStyle   = "rgba(" + backgroundColor.r + "," + backgroundColor.g + "," + backgroundColor.b + "," + backgroundColor.a + ")";
-		context.strokeStyle = "rgba(" + borderColor.r + "," + borderColor.g + "," + borderColor.b + "," + borderColor.a + ")";
-
-		context.lineWidth = borderThickness;
-
+		context.textAlign = 'center'
 		context.fillStyle = "rgba("+textColor.r+", "+textColor.g+", "+textColor.b+", 1.0)";
-		context.fillText( message, borderThickness, fontsize + borderThickness);
-
+		context.fillText( message, size/2, size/2 );
+		
 		var texture = new THREE.Texture(canvas)
 		texture.needsUpdate = true;
 
