05256233e45ab2ee6c8c6795efc776bac8a896e1
unknown <mtraylor@mines.edu>
2015-05-20 10:32:56 -0600

    example file being rendered with View.js

--- scripts/View.js
+++ scripts/View.js
@@ -17,7 +17,7 @@ var colors = {
 function View(property){
 
 	var container, stats;
-	var camera, scene, controls, raycaster, renderer;
+	var camera, cameraOrtho, scene, controls, raycaster, renderer;
 
 	var mouse = new THREE.Vector2();
 	var INTERSECTED;
@@ -29,11 +29,16 @@ function View(property){
 		container = document.createElement('div');
 		document.body.appendChild(container);
 
+		var width = window.innerWidth;		//used to make the camera declarations prettier
+		var height = window.innerHeight;
 		var maxDimension = Math.max(property.box.size.x, property.box.size.y, property.box.size.z);
-		camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, maxDimension*7);
+		camera = new THREE.PerspectiveCamera(45, width / height, 0.1, maxDimension*7000);
 		camera.up.set(0,0,1);
 		camera.position.set(Math.max(maxDimension/5, property.box.size.x), Math.max(maxDimension/5,property.box.size.y), Math.max(maxDimension/5, property.box.size.z));
 		camera.lookAt(property.box.center);
+
+		cameraOrtho = new THREE.OrthographicCamera( width/-2, width/2, height/2, height/-2, 1, 1000);
+
 
 		scene = new THREE.Scene;
 		scene.add(new THREE.AmbientLight(colors.soft_white));
@@ -42,7 +47,7 @@ function View(property){
 		controls.zoomSpeed = 3.0;
 		controls.minDistance = 1;
 		controls.maxDistance = maxDimension * 2;
-		//controls.target = new THREE.Vector3(property.box.center);
+		controls.target = property.box.center;
 
 		renderer = new THREE.WebGLRenderer({antialias: true});
 		renderer.setSize(window.innerWidth, window.innerHeight);
@@ -53,6 +58,7 @@ function View(property){
 
 		var property_boundary = new THREE.Mesh(new THREE.BoxGeometry(property.box.size.x, property.box.size.y, property.box.size.z));
 		var box = new THREE.EdgesHelper(property_boundary, colors.axes);
+		box.applyMatrix(new THREE.Matrix4().makeTranslation(property.box.center.x, property.box.center.y, property.box.center.z));
 
 		var cylinder_geometry = new THREE.CylinderGeometry(1, 1, 3, 10);
 		var cylinder_material = new THREE.MeshBasicMaterial({ color: colors.pink });
@@ -62,10 +68,19 @@ function View(property){
 
 		scene.add(box);
 
-		var box = new THREE.Mesh(new THREE.BoxGeometry(2, 2, 2), new THREE.MeshBasicMaterial({wireframe: true}));
-		scene.add(box);
-
+		addSurveyLines();
 		
+	}
+
+	function addSurveyLines(){
+		var material = undefined;//new THREE.LineBasicMaterial({color:colors.black});
+		property.holes.forEach(function(hole){
+			geometry = new THREE.Geometry();
+			hole.surveyPoints.forEach(function(point){
+				geometry.vertices.push(point);
+			});
+			scene.add(new THREE.Line(geometry, material));
+		});
 	}
 
 	render();
