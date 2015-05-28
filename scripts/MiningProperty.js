aa3526a8ec411007c421f46df89bada3201704dc
Chris <chrbutle@mines.edu>
2015-05-28 10:35:32 -0600

    Updated for format v2
    We added support for format v2, but we don't take advantage of the updated
    color information.

--- scripts/MiningProperty.js
+++ scripts/MiningProperty.js
@@ -2,6 +2,9 @@
 //   variable names it can't find declared.
 /* global THREE */
 
+// This variable is used to enable error checks when JSON is loading.
+var enableErrorChecks = false;
+
 function loadJSON(url) {
 	var json = null;
 	$.ajax({
@@ -16,11 +19,22 @@ function loadJSON(url) {
 	return json;
 }
 
+function printCallStackIfFalse(predicate, msg) {
+	if (predicate) {
+		return;
+	}
+	console.error(msg);
+	console.trace();
+}
+
 // Convert [a, b, c, d..] into {x: a, y: b, z: c}.
 //   Disregard anything after the third element.
 //   Anything missing is assumed to be 0.
 function vec3FromArray(array) {
+	printCallStackIfFalse(array != undefined,
+		"'array' wasn't defined when we expected it to be.");
 	return new THREE.Vector3(array[0], array[1], array[2]);
+
 }
 
 // Take a single hole, as it exists in Newmont's JSON under the "holes" tag,
@@ -32,64 +46,23 @@ function parseHoleData(jsonHole) {
 	// The metadata can be used for tooltips and debuggin.
 	hole.id = jsonHole["id"];
 	hole.name = jsonHole["name"];
+	hole.traceColor = jsonHole["traceColor"];
 
-	// Mineral deposits are saved with their concentration and depth down
-	//   the hole, instead of coorinates. We'll need to calculate the
-	//   coordinates from a given depth, or look it up in a cache.
-	var depthMap = {};
-	var depthToCoords = function (depth) {
-
-		//iterate until we find a depth in our hole greater than or equal to 
-		//the mineral's location
-		var index = 1;
-		while(depths[index] < depth && index < depths.length){
-			index+=1;
-		}
-
-		index -= 1;
-		var lastPoint = depthMap[depths[index]];
-
-		var depthDiff = depth - depths[index];
-		var pos = lastPoint.location.clone();
-		pos.x += depthDiff * Math.cos(lastPoint.inclination) * Math.sin(lastPoint.azimuth);
-		pos.y += depthDiff * Math.cos(lastPoint.inclination) * Math.cos(lastPoint.azimuth);
-		pos.z += depthDiff * Math.sin(lastPoint.inclination);
-		return pos;
-	};
 
 	// Process the survey hole.
 	// We do this by making a list of points, which we'll connect later
 	//   using OpenGL.
 	// We assume any individual hole's survey line is continuous,
 	//   *and in order*. If they're not in order, bad things happen (TM).
-	var surveys = jsonHole["downholeSurveys"];
+	var surveys = jsonHole["interpolatedDownholeSurveys"];
 	hole.surveyPoints = [];
 	for (var i = 0; i < surveys.length; i += 1 ) {
 
-		var location = surveys[i]["location"];
-		hole.surveyPoints.push(vec3FromArray(location));
-		depthMap[surveys[i]["depth"]] = {
-			azimuth:     surveys[i]["azimuth"]/180 * Math.PI,
-			inclination: surveys[i]["inclination"]/180 * Math.PI,
-			location:    vec3FromArray(location)
-		};
+		var location = vec3FromArray(surveys[i]["location"]);
+		hole.surveyPoints.push(location);
 	}
 
-	var depths = Object.keys(depthMap);
-	depths.sort(function(a,b){return a - b});
 
-	/*
-	// Add the end point to the survey hole.
-	var lastJSON = surveys[surveys.length - 1];
-	var last = vec3FromArray(lastJSON["location"]);
-	last.x += Math.cos(lastJSON["inclination"]) * Math.cos(lastJSON["azimuth"]);
-	last.y += Math.cos(lastJSON["inclination"]) * Math.sin(lastJSON["azimuth"]);
-	last.z += -lastJSON["depth"];
-
-	depthMap[maxDepth] = last;
-	hole.surveyPoints.push(last);
-	
-	*/
 	// And then the intervals with mineral deposits.
 	hole.minerals = [];
 	var mineralsJSON = jsonHole["downholeDataValues"];
@@ -97,15 +70,31 @@ function parseHoleData(jsonHole) {
 		var intervals = [];
 
 		var intervalsJSON = mineralsJSON[i]["intervals"];
-		for (var k = 0; k < intervalsJSON.length; k += 1) {
-			intervals.push({
-				value: intervalsJSON[k]["value"],
-				// The intervals are stored by depth in the hole,
-				//   not the coordinates. We need to adjust that to use them.
-				start: depthToCoords(intervalsJSON[k]["from"]),
-				end  : depthToCoords(intervalsJSON[k]["to"])
-			});
-		}
+		intervalsJSON.forEach( function(intervalJSON) {
+
+			interval = {
+				value: intervalJSON["value"],
+
+				// We are only storing depth for testing consistency.
+				depth: {
+					start: intervalJSON["from"],
+					end  : intervalJSON["to"]
+				},
+				path: {
+					start:  vec3FromArray(intervalJSON["path"][0]),
+					end  :  vec3FromArray(intervalJSON["path"][1])
+				}
+			};
+
+			// If error checking is enabled, make sure that the azimuth and
+			//   inclination match up with the given path.
+			if (enableErrorChecks) {
+				// TODO: check for data consistency.
+			}
+
+			intervals.push(interval);
+
+		});
 
 		hole.minerals.push({
 			type: mineralsJSON[i]["name"],
@@ -115,7 +104,6 @@ function parseHoleData(jsonHole) {
 
 	return hole;
 }
-
 
 /*
 This the structure of a MiningProperty object.
@@ -149,57 +137,51 @@ This the structure of a MiningProperty object.
 }
 */
 function MiningPropertyFromJSON(propertyJSON) {
-	this.name = propertyJSON["projectName"];
+	if (propertyJSON["formatVersion"] != 0.02) {
+		console.error("Unsupported format version: " + propertyJSON["formatVersion"]);
+		return null;
+	}
+
+	this.name        = propertyJSON["projectName"];
 	this.description = propertyJSON["description"];
-	this.projectionEPSG = propertyJSON["projectionEPSG"]; // Some sort of coordinate.
+	this.epsg        = propertyJSON["projectionEPSG"];
 
 	var boxMin = vec3FromArray(propertyJSON["boxMin"]);
 	var boxMax = vec3FromArray(propertyJSON["boxMax"]);
 
-	// We need to adjust each point so that the bottom corner is at (0, 0, 0).
-	// This is our offset for that.
-	var offset = boxMin.clone();
+	var originShift = propertyJSON["originShift"];
 
-	var size   = boxMax.clone().sub(boxMin);
-	var center = size.clone().multiplyScalar(0.5);
-
+	var size = boxMax.clone().sub(boxMin);
+	var center = size.clone().multiplyScalar(0.5).add(boxMin);
 	this.box = {
 		size:   size,
-		center: center,
+		center: center
 	};
 
-	this.holes = [];
-	var holesJSON = propertyJSON["holes"];
-	for (var i = 0; i < holesJSON.length; i += 1) {
-		this.holes.push(parseHoleData(holesJSON[i]));
-	}
+	this.longLatMin = vec3FromArray(propertyJSON["longLatMin"]);
+	this.longLatMax = vec3FromArray(propertyJSON["longLatMax"]);
 
-	var intervals = 0;
+	this.desurveyMethod = propertyJSON["MidpointSplit"];
 
-	// Go through and adjust each point.
-	this.holes.forEach(function (hole) {
-		hole.surveyPoints.forEach(function (point) {
-			point.sub(offset);
-		});
+	this.analytes = propertyJSON["analytes"];
 
-		hole.minerals.forEach(function (mineral) {
-			mineral.intervals.forEach(function (interval) {
-				intervals += 1;
-				interval.start.sub(offset);
-				interval.end.sub(offset);
-			});
-		});
+	var holes = [];
+	propertyJSON["holes"].forEach(function (hole) {
+		holes.push(parseHoleData(hole));
 	});
+	this.holes = holes;
 
-	console.log("Found " + this.holes.length + " holes.");
-	console.log("Found " + intervals + " intervals.");
-};
+	var expectedHoles = propertyJSON["numHoles"];
+	if (this.holes.length != expectedHoles) {
+		console.error("Expected " + expectedHoles + "holes, but found " + this.holes.length + " holes.");
+	}
+}
 
 function miningPropertyFromURL(url, onError) {
 	if (!onError) {
 		onError = function() {
-			console.log("[Error] Something went wrong when trying to load MiningProperty JSON from\"",
-				url, "\".");
+			console.error("Something went wrong when trying to load MiningProperty JSON from URL:" +
+			              "\n\t" + url, ".");
 		};
 	}
 	var data = loadJSON(url);
