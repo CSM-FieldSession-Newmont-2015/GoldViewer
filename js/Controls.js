485dbd560ebd343ce0d0c48344bf86b0de54d2af
Joseph Stockwell <jostockw@mines.edu>
2015-06-12 10:12:29 -0600

    Fixed some sidebar issues and formatting

--- js/Controls.js
+++ js/Controls.js
@@ -88,7 +88,20 @@ function loadControls() {
 }
 
 function loadSidebar() {
-	$.get("html/Sidebar.html", function (data) {
+    $('.sidebar-container').click(function (e) {
+        if ($(this).width() - e.pageX > 20)
+            return;
+
+        $('#sidebar').toggle('slide', { direction: 'left' }, function () {
+            if ($('#sidebar').css('display') == 'none') {
+                $('.sidebar-container').width(20);
+            } else {
+                $('.sidebar-container').width($('#sidebar').width() + 20);
+            }
+        });
+    });
+
+    $.get("html/Sidebar.html", function (data) {
 		$("#sidebar").append(data);
 	});
 }
