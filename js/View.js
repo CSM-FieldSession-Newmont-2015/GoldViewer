f90103d30b08574c5a6f317c6edf9e9f84b35f3b
unknown <mtraylor@mines.edu>
2015-06-14 12:04:57 -0600

    moving the camera along with the reticle

--- js/View.js
+++ js/View.js
@@ -1406,6 +1406,7 @@ function View(projectURL) {
 
 			sceneOrtho.remove(tooltipSprite);
 			scene.remove(intersected);
+			intersected = null;
 
 			window.clearTimeout(mouseTimeout);
 			if(event.buttons == 0 && raycaster){
@@ -1472,14 +1473,38 @@ function View(projectURL) {
 		tempVec1.multiplyScalar(-1 * reticle.geometry.boundingSphere.radius);
 		movementVector.add(tempVec1);
 
+		var acceleration = length / 50000 + 0.01;
+
+		var reticleMotion = getDeltasForMovement(movementVector, acceleration);
+		var cameraMotion = getDeltasForMovement(movementVector, acceleration * 0.7);
+
+		//get rid of the last interval, in case it exists
+		window.clearInterval(motionInterval);
+
+		//Start an interval to move the reticle around!
+		//Trigger 100 times a second
+		motionInterval = setInterval(function(){
+			if(reticleMotion.length != 0){
+				controls.target.add(reticleMotion.pop());
+			}
+			if(cameraMotion.length != 0){
+				camera.position.add(cameraMotion.pop());
+			}
+
+			if(reticleMotion.length === 0 && cameraMotion.length === 0){
+				window.clearInterval(motionInterval);
+				motionInterval = null;
+			}
+		}, 10);
+	}
+
+	function getDeltasForMovement(movementVector, acceleration){
 
 		//get the total length of the movement
 		var length = movementVector.length();
 
 		//now construct a motion array of Vector3's that will constantly
 		//accelerate and then decelerate towards the object.
-
-		var acceleration = length / 50000 + 0.01;
 		var normalMovement = movementVector.clone();
 		normalMovement.normalize();
 		var totalMovement = 0;
@@ -1511,18 +1536,7 @@ function View(projectURL) {
 		normalMovement.multiplyScalar(length - totalMovement);
 		motion.push(normalMovement);
 
-		//get rid of the last interval, in case it exists
-		window.clearInterval(motionInterval);
-
-		//Start an interval to move the reticle around!
-		//Trigger 100 times a second
-		motionInterval = setInterval(function(){
-			controls.target.add(motion.pop());
-			if(motion.length === 0){
-				window.clearInterval(motionInterval);
-				motionInterval = null;
-			}
-		}, 10);
+		return motion;
 	}
 
 	function addBoundingBox() {
