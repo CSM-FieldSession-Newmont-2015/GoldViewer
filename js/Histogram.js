d58bb92ef54da7811c627166b6468b9b8052a0f6
Joseph Stockwell <jostockw@mines.edu>
2015-06-14 14:41:50 -0600

    Asthetics

--- js/Histogram.js
+++ js/Histogram.js
@@ -10,7 +10,8 @@
 
 	for (var mineral in minerals) {
 		var div = $('<div class="mineral-container">').appendTo('.minerals');
-		div.append('<input id="cb' + mineral + '" type="checkbox" data-mineral="' + mineral + '"><label>' + mineral + '</label>');
+		div = $('<span>').appendTo(div);
+		div.append('<input id="cb' + mineral + '" type="checkbox" data-mineral="' + mineral + '"><h2><label for="cb' + mineral + '">' + mineral + '</label></h2>');
 		$('<svg id="svg' + chartIndex + '" class="chart">').appendTo(div);
 		$('#cb' + mineral).prop('checked', true);
 		$('#cb' + mineral).click(callToggleVisibile);
@@ -31,7 +32,7 @@
 		var margin = {
 				top: 10,
 				right: 30,
-				bottom: 50,
+				bottom: 75,
 				left: 30
 			},
 			width = 400 - margin.left - margin.right,
@@ -39,7 +40,8 @@
 
 		var x = d3.scale.linear()
 			.domain([d3.min(values), d3.max(values)])
-			.range([0, width]);
+			.range([0, width])
+			.nice(intervals);
 
 		// Generate a histogram using uniformly-spaced bins.
 		var intervals = 20;
@@ -66,7 +68,15 @@
 			.append("g")
 			.attr("transform", "translate(" +
 				margin.left + "," + margin.top + ")");
-
+/*
+		svg.append("text")
+        .attr("x", (width / 2))
+        .attr("y", 0 - (margin.top / 2))
+        .attr("text-anchor", "middle")
+        .style("font-size", "16px")
+        .style("text-decoration", "underline")
+        .text("Value vs Date Graph");
+*/
 		var bar = svg.selectAll(".bar")
 			.data(data)
 			.enter().append("g")
